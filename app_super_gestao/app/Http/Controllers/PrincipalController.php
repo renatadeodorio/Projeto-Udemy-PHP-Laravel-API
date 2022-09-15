55e04b3b49d48c2650f7de70f4ffa251c097de37
Renata Deodório <renatadeod@gmail.com>
2022-09-15 18:22:32 -0300

    Ajustes Controllers

--- app_super_gestao/app/Http/Controllers/PrincipalController.php
+++ app_super_gestao/app/Http/Controllers/PrincipalController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PrincipalController extends Controller
 {
     public function principal() {
-        echo 'Olá, seja Bem-vindo!';
+        return view('site.principal');
     }
 }
