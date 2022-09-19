f9b03c612183f8604ad1b0d4b1b13fe3086ffa89
Renata Deodório <renatadeod@gmail.com>
2022-09-19 09:27:13 -0300

    Criando Menu de navegação

--- app_super_gestao/resources/views/site/principal.blade.php
+++ app_super_gestao/resources/views/site/principal.blade.php
@@ -1,1 +1,13 @@
-<h3>Olá, Seja Bem-vindo! (view)</h3>
+<h3>Principal (view)</h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+    <li>
+        <a href="/sobre-nos">Sobre Nós</a>
+    </li>
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+</ul>
