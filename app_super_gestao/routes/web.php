b610f4aa562dfb5e7a64d9d06ccb3cff64e6e486
Renata Deodório <renatadeod@gmail.com>
2022-09-20 10:11:45 -0300

    Novas Rotas

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -9,5 +9,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 
 Route::get('/sobrenos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
-
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/Login', [LoginController::class, 'login'])->name('site.login');
+
+//app
+Route::prefix('/app')->group(function () {
+   Route::get('/clientes', function() { return 'Clientes'; });
+   Route::get('/fornecedores', function() { return 'Fornecedores'; });
+   Route::get('/produtos', function() { return 'produtos'; });
+
+});
