4c82958ea459c8a3c117d2cf34d856f8ae35dcda
Renata Deodório <renatadeod@gmail.com>
2022-09-21 16:26:30 -0300

    Rota de contigêencia(fallback)

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -16,5 +16,17 @@ Route::prefix('/app')->group(function () {
    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
    Route::get('/fornecedores', function() { return 'Fornecedores'; })->name('app.fornecedores');
    Route::get('/produtos', function() { return 'produtos'; })->name('app.produtos');
+});
+
+Route::get('/rota1', function() {
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+Route::fallback(function () {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
 
 });
