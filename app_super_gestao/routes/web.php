43327301537abf743de1f9845b9788cd05d0591d
Renata Deodório <renatadeod@gmail.com>
2022-09-22 17:38:27 -0300

    Rota e view Fornecedor

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\FornecedorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
@@ -14,11 +16,9 @@ Route::get('/login', [LoginController::class, 'Login'])->name('site.login');
 //app
 Route::prefix('/app')->group(function () {
    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
-   Route::get('/fornecedores', function() { return 'Fornecedores'; })->name('app.fornecedores');
+   Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
    Route::get('/produtos', function() { return 'produtos'; })->name('app.produtos');
 });
-
-Route::get('/teste{p1}/{p2}', 'TesteController')->name('teste');
 
 Route::fallback(function () {
     echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
