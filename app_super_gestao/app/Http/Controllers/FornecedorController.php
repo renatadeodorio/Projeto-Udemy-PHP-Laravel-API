2b35b182a5848090e878415749f47c7e4ae18d0f
Renata Deodório <renatadeod@gmail.com>
2022-09-26 09:58:03 -0300

    Blade @isset

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index() {
-        return view('app.fornecedor.index');
+        $fornecedores = [
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '00.000.000/000-00'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S'
+            ]
+            ];
+
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -1,15 +1,13 @@
 <h3>Fornecedor</h3>
 
 
-Fornecedores: {{ $fornecedores[0]['nome'] }}
-<br>
-Status: {{ $fornecedores[0]['status'] }}
-<br>
-@if( !($fornecedores[0]['status'] == 'S') )
-   Fornecedor inativo
-@endif
-<br>
-@unless( $fornecedores[0]['status'] == 'S')
-    Fornecedor inativo
-    @endunless
-    <br>
+@isset($fornecedores)
+
+  Fornecedor: {{ $fornecedores[0]['nome'] }}
+  <br>
+  Status: {{ $fornecedores[0]['status'] }}
+  <br>
+  @isset( $fornecedores[0]['cnpj'])
+    CNPJ: {{ $fornecedores[0]['cnpj'] }}
+  @endisset
+@endisset
