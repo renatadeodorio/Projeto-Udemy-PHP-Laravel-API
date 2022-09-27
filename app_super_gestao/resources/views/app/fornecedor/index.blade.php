9eda9ec75145356534bb4424b0d84acb6f26e14e
Renata Deodório <renatadeod@gmail.com>
2022-09-27 14:49:06 -0300

    Blade @Switch/Case

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -7,6 +7,20 @@
   <br>
   Status: {{ $fornecedores[1]['status'] }}
   <br>
-  CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Dado não foi preenchido' }}
-
+  CNPJ: {{ $fornecedores[1]['cnpj'] ?? '' }}
+  <br>
+  Telefone: ({{ $fornecedores[1]['ddd'] ?? '' }}) {{ $fornecedores[1]['telefone'] ?? '' }}
+  @switch ($fornecedores[1]['ddd'])
+      @case ('11')
+         São Paulo - SP
+         @break
+      @case ('32')
+         Juiz de Fora - MG
+         @break
+      @case ('85')
+         Fortaleza - CE
+         @break
+      @default
+         Estado não identificado
+    @endswitch
 @endisset
