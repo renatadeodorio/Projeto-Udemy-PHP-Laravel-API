e7d4b0a41b2e68680b4a6ad61a8b9efefff0c568
Renata Deodório <renatadeod@gmail.com>
2022-09-27 15:16:51 -0300

    Blade @while

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -3,7 +3,8 @@
 
 @isset($fornecedores)
 
-  @for($i = 0; isset($fornecedores[$i]); $i++)
+  @php $i = 0 @endphp
+  @while(isset($fornecedores[$i]))
     Fornecedor: {{ $fornecedores[$i]['nome'] }}
     <br>
     Status: {{ $fornecedores[$i]['status'] }}
@@ -12,7 +13,8 @@
     <br>
     Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[1]['telefone'] ?? '' }}
     <hr>
-  @endfor
+    @php $i++ @endphp
+  @endwhile
 @endisset
 
 
