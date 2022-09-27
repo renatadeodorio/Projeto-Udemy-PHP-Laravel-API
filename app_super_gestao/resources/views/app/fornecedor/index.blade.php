11f67f6ad9191951b425b0494a44da4fd3632e7b
Renata Deodório <renatadeod@gmail.com>
2022-09-27 15:08:24 -0300

    Blade @for

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -3,24 +3,16 @@
 
 @isset($fornecedores)
 
-  Fornecedor: {{ $fornecedores[1]['nome'] }}
-  <br>
-  Status: {{ $fornecedores[1]['status'] }}
-  <br>
-  CNPJ: {{ $fornecedores[1]['cnpj'] ?? '' }}
-  <br>
-  Telefone: ({{ $fornecedores[1]['ddd'] ?? '' }}) {{ $fornecedores[1]['telefone'] ?? '' }}
-  @switch ($fornecedores[1]['ddd'])
-      @case ('11')
-         São Paulo - SP
-         @break
-      @case ('32')
-         Juiz de Fora - MG
-         @break
-      @case ('85')
-         Fortaleza - CE
-         @break
-      @default
-         Estado não identificado
-    @endswitch
+  @for($i = 0; isset($fornecedores[$i]); $i++)
+    Fornecedor: {{ $fornecedores[$i]['nome'] }}
+    <br>
+    Status: {{ $fornecedores[$i]['status'] }}
+    <br>
+    CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
+    <br>
+    Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[1]['telefone'] ?? '' }}
+    <hr>
+  @endfor
 @endisset
+
+
