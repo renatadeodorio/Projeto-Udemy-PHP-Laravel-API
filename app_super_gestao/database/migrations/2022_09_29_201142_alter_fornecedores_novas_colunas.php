46b6d792163acf9c1b7a11868311aff3eb1f4060
Renata Deodório <renatadeod@gmail.com>
2022-09-29 17:25:33 -0300

    Adicionando campos a uma tabela

--- app_super_gestao/database/migrations/2022_09_29_201142_alter_fornecedores_novas_colunas.php
+++ app_super_gestao/database/migrations/2022_09_29_201142_alter_fornecedores_novas_colunas.php
@@ -13,10 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('fornecedores', function (Blueprint $table) {
-            $table->id();
-            $table->string('nome', 50);
-            $table->timestamps();
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->string('uf', 2);
+            $table->string('email', 150);
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('fornecedores');
+        //
     }
 };
