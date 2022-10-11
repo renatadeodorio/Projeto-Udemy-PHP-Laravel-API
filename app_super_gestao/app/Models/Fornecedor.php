f777e3c6014d5f140d4c4cad9cf11c7e571a313e
Renata Deodório <renatadeod@gmail.com>
2022-10-11 18:57:59 -0300

    Soft Delete

--- app_super_gestao/app/Models/Fornecedor.php
+++ app_super_gestao/app/Models/Fornecedor.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+//fornecedores
 
 class Fornecedor extends Model
 {
+    //
+    use SoftDeletes;
+
     protected $table = 'fornecedores';
     protected $fillable = ['nome','site','uf','email'];
 }
