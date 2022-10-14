22ac06c8656014de5bbdf74fbc916a0c49678ca0
Renata Deodório <renatadeod@gmail.com>
2022-10-14 20:32:23 -0300

    Validação campos obrigatórios (required)

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -11,4 +11,15 @@ class ContatoController extends Controller
         var_dump($_POST);
         return view('site.contato', ['titulo' => 'Contato']);
     }
+
+    public function salvar(Request $request) {
+
+        $request->validate([
+            'nome' => 'required',
+            'telefone' => 'required',
+            'email' => 'required',
+            'motivo_contato' => 'required',
+            'mensagem' => 'required'
+        ]);
+}
 }
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
 Route::get('/sobrenos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
-Route::post('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 Route::get('/login', [LoginController::class, 'Login'])->name('site.login');
 
 //app
